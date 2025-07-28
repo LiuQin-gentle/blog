328b50c9922d76666aa855a8ff19107009d58f1e
v_liuqin05 <v_liuqin05@baidu.com>
2025-07-28 11:27:46 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: v_liuqin05 v_liuqin05@baidu.com
+ * @Date: 2025-07-28 10:48:38
+ * @LastEditors: v_liuqin05 v_liuqin05@baidu.com
+ * @LastEditTime: 2025-07-28 10:58:02
+ * @FilePath: /vue-test/src/App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <main>
+    <HelloWorld msg="hello" />
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+ <div>{{ msg }}</div>
+</template>
+
+<style scoped>
+
+</style>
