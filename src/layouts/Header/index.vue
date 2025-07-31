2b9fe319b190320e1f0fe86e61655dfc94a3149e
v_liuqin05 <v_liuqin05@baidu.com>
2025-07-31 11:32:12 +0800

    feat: 安装less

---
+++ src/layouts/Header/index.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Author: v_liuqin05
+ * @Date: 2025-07-30 17:19:19
+ * @LastEditors: v_liuqin05
+ * @LastEditTime: 2025-07-31 11:09:30
+ * @Description: 页面 header
+-->
+ <template>
+    <div class="header">
+        <div class="title">
+        <img src="../../assets/img/blogger.png" style="width: 30px; height: 30px;margin-right: 10px;"></img>
+        <span>Blog</span>
+    </div>
+    </div>
+ </template>
+ <style scoped lang="less">
+ .header {
+    .title {
+        padding: 15px 0;
+        display: flex;
+        align-items: center;
+        font-size: 20px;
+    }
+ }
+ </style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,16 +2,17 @@
  * @Author: v_liuqin05
  * @Date: 2025-07-30 16:58:57
  * @LastEditors: v_liuqin05
- * @LastEditTime: 2025-07-30 17:11:21
+ * @LastEditTime: 2025-07-31 11:31:28
  * @Description: 首页
 -->
 <script setup>
+import Header from '@/layouts/Header/index.vue';
 </script>
 
 <template>
   <div class="common-layout">
     <el-container>
-      <el-header>Header</el-header>
+      <el-header style="border: 1px solid #E4E7ED"><Header/></el-header>
       <el-container>
         <el-aside width="200px">Aside</el-aside>
         <el-main style="height: calc(100vh - 60px)">Main</el-main>
